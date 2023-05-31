550ba3095b375fe0e7699b95af86207c60f5c4d9
Nycole Mendonça <nycolemendonca4@gmail.com>
2023-05-31 18:31:43 -0300

    add github-finder

---
+++ github-finder/src/components/Search.tsx
@@ -0,0 +1,37 @@
+type SearchProps = {
+  loadUser: (userName: string) => Promise<void>;
+}
+
+import { useState, KeyboardEvent } from 'react';
+import { GoSearch } from 'react-icons/go';
+
+import classes from './Search.module.css';
+
+const Search = ({loadUser}: SearchProps) => {
+  const [userName, setUserName] = useState('');
+
+  const handleKeyDown = (e: KeyboardEvent) => {
+    if (e.key === 'Enter') {
+      loadUser(userName)
+    }
+  }
+
+  return (
+    <div className={classes.search}>
+      <h2>Search for a user:</h2>
+      <p>Discover your best projects</p>
+
+      <div className={classes.inputField}>
+        <input 
+          type='text' 
+          placeholder='Enter username'
+          onChange={(e) => setUserName(e.target.value)} 
+          onKeyDown={handleKeyDown}
+        />
+        <button onClick={() => loadUser(userName)}><GoSearch/></button>
+      </div>
+    </div>
+  )
+}
+
+export default Search
---
+++ github-finder/src/components/User.tsx
@@ -0,0 +1,37 @@
+import { UserProps } from "../types/user";
+
+import { MdLocationPin } from 'react-icons/md';
+
+import { Link } from 'react-router-dom';
+
+import classes from './User.module.css';
+
+const User = ({avatar_url, login, location, followers, following}: UserProps) => {
+  return (
+    <div className={classes.user}>
+      <img src={avatar_url} alt={login}/>
+      <h2>{login}</h2>
+      { location && (
+        <p className={classes.location}>
+          <MdLocationPin/>
+          <span>{location}</span>
+        </p>
+      )}
+      <div className={classes.followField}>
+        <div className={classes.follow}>
+          <p className={classes.followTitle}>Followers:</p>
+          <p>{followers}</p>
+        </div>
+        <div className={classes.follow}>
+          <p className={classes.followTitle}>Following:</p>
+          <p>{following}</p>
+        </div>
+      </div>
+
+      {/* Resolve - Redirect Error */}
+      <Link to={`/repos/${login}`} className={classes.button}>See more projects</Link>
+    </div>
+  )
+}
+
+export default User
